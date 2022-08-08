be074ae8f80dbfe89a99f810468a45df2fb73bb4
Carlos Azaustre <cazaustre@gmail.com>
2022-08-08 10:23:48 +0200

    feat: :tada:

---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Heading = ({ size = "md", text = "" }) => {
+  return <h1>{text}</h1>;
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Heading } from "components";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Heading text="Hola Mundo!" />
   </React.StrictMode>
 );
 
